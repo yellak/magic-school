2e955e21ee4d0125e999bbfcba882d908cf1459d
Kalley Wilkerson <kalleywra@gmail.com>
2020-01-21 23:49:13 -0300

    CMake working withou importing my own libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Here I am" << std::endl;
+    sf::RenderWindow window;
+    window.create(sf::VideoMode(500, 500), "Test window");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case sf::Event::Closed:
+                window.close();
+                break;
+            
+            default:
+                break;
+            }
+        }
+        
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+    
     return 0;
 }
